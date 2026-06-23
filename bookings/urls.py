a5d9db8b76c85127fcddace07992862d9ca72f69
Victor <victorfariasbezerra12@gmail.com>
2026-06-23 00:05:58 -0300

    disable already selected time

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import AppointmentCreateView, AppointmentSuccessView
+from .views import AppointmentCreateView, AppointmentSuccessView, BookedTimesView
 
 urlpatterns = [
     path('book/', AppointmentCreateView.as_view(), name="appointment_create"),
     path("booking/success/", AppointmentSuccessView.as_view(), name="appointment_success"),
+
+    path("booked-times/", BookedTimesView.as_view(), name="booked_times"),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import AppointmentForm
+from .models import Appointment
+from django.http import JsonResponse
 
 # Create your views here.
 class AppointmentCreateView(View):
@@ -20,3 +22,15 @@ class AppointmentCreateView(View):
 class AppointmentSuccessView(View):
     def get(self, request):
         return render(request, "bookings/appointment_success.html")
+    
+class BookedTimesView(View):
+    def get(self, request):
+        appointment_date = request.GET.get("date")
+
+        booked_times = Appointment.objects.filter(
+            appointment_date=appointment_date
+        ).values_list("appointment_time", flat=True)
+
+        data = [time.strftime("%H:%M:%S") for time in booked_times]
+
+        return JsonResponse({"booked_times": data})
